673ce550faa9f968d80e04f2f71b434cc09a092a
Luan Victor <lvictor.dutra@gmail.com>
2025-05-22 18:11:26 -0300

    feat: implement PartController with methods to retrieve parts by ID and name

---
+++ internal/http/controllers/part_controller.go
@@ -0,0 +1,57 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/Luzin7/pcideal-be/internal/http/services"
+	"github.com/gin-gonic/gin"
+)
+
+type PartController struct {
+	PartService *services.PartService
+}
+
+func NewPartController(partService *services.PartService) *PartController {
+	return &PartController{PartService: partService}
+}
+
+func (pc *PartController) GetAllParts(c *gin.Context) {
+	parts, err := pc.PartService.GetAllParts()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao buscar peças"})
+		return
+	}
+
+	c.JSON(http.StatusOK, parts)
+}
+
+func (pc *PartController) GetPartByID(c *gin.Context) {
+	id := c.Param("id")
+	part, err := pc.PartService.GetPartByID(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao buscar peça"})
+		return
+	}
+
+	if part == nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Peça não encontrada"})
+		return
+	}
+
+	c.JSON(http.StatusOK, part)
+}
+func (pc *PartController) GetPartByName(c *gin.Context) {
+	name := c.Param("name")
+	part, err := pc.PartService.GetPartByName(name)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao buscar peça"})
+		return
+	}
+
+	if part == nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Peça não encontrada"})
+		return
+	}
+
+	c.JSON(http.StatusOK, part)
+}
