3fee4e6ffd8c46a04b27c6e6cd1d5b45c804b745
Luan Victor <lvictor.dutra@gmail.com>
2025-05-22 18:11:22 -0300

    feat: add GetPartByID and GetPartByName methods to PartRepository

--- infra/repositories/part_repository.go
+++ infra/repositories/part_repository.go
@@ -49,3 +49,37 @@ func (partRepository *PartRepository) GetAllParts() ([]*models.Part, error) {
 
 	return parts, nil
 }
+
+func (partRepository *PartRepository) GetPartByID(id string) (*models.Part, error) {
+	ctx := context.TODO()
+
+	var part models.Part
+
+	err := partRepository.collection.FindOne(ctx, bson.M{"_id": id}).Decode(&part)
+
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &part, nil
+}
+
+func (partRepository *PartRepository) GetPartByName(name string) (*models.Part, error) {
+	ctx := context.TODO()
+
+	var part models.Part
+
+	err := partRepository.collection.FindOne(ctx, bson.M{"name": name}).Decode(&part)
+
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &part, nil
+}
