2752ed21eca4b160f50c0d3220d4d3fb799fa811
Luan Victor <lvictor.dutra@gmail.com>
2025-05-23 01:35:07 -0300

    feat: update PartContract interface to rename GetPartByName to GetPartByModel and ensure method consistency

--- internal/contracts/part_contract.go
+++ internal/contracts/part_contract.go
@@ -3,7 +3,9 @@ package contracts
 import "github.com/Luzin7/pcideal-be/internal/core/models"
 
 type PartContract interface {
+	CreatePart(part *models.Part) error
 	GetAllParts() ([]*models.Part, error)
 	GetPartByID(id string) (*models.Part, error)
-	GetPartByName(name string) (*models.Part, error)
+	GetPartByModel(model string) (*models.Part, error)
+	UpdatePart(partId string, part *models.Part) error
 }
