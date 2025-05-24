9b4f6339b29210be518dfe46a23fe47b57ec6ef9
Luan Victor <lvictor.dutra@gmail.com>
2025-05-24 17:40:33 -0300

    fix: change ID type in Price struct to primitive.ObjectID and update NewPrice method

--- internal/core/models/price.go
+++ internal/core/models/price.go
@@ -7,19 +7,19 @@ import (
 )
 
 type Price struct {
-	ID          string    `bson:"_id" json:"id"`
-	PartID      string    `bson:"part_id" json:"part_id"`
-	Price       int64     `bson:"price_cents" json:"price_cents"`
-	Store       string    `bson:"store" json:"store"`
-	LastUpdated time.Time `bson:"last_updated" json:"last_updated"`
+	ID        primitive.ObjectID `bson:"_id" json:"id"`
+	PartID    string             `bson:"part_id" json:"part_id"`
+	Price     int64              `bson:"price_cents" json:"price_cents"`
+	Store     string             `bson:"store" json:"store"`
+	UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
 }
 
 func NewPrice(partID string, price int64, store string) *Price {
 	return &Price{
-		ID:          primitive.NewObjectID().Hex(),
-		PartID:      partID,
-		Price:       price,
-		Store:       store,
-		LastUpdated: time.Now(),
+		ID:        primitive.NewObjectID(),
+		PartID:    partID,
+		Price:     price,
+		Store:     store,
+		UpdatedAt: time.Now(),
 	}
 }
