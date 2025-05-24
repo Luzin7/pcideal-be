dfa931a9cf510b896e1fad661187c7f421de6759
Luan Victor <lvictor.dutra@gmail.com>
2025-05-24 16:52:09 -0300

    refactor: remove unused times package and constants

--- pkg/times/times.go
+++
@@ -1,9 +0,0 @@
-package times
-
-const (
-	OneSecondInMilliseconds = 1000
-	OneMinuteInSeconds      = 60
-	OneHourInMinutes        = 60
-	OneHourInMilliseconds   = OneSecondInMilliseconds * OneHourInMinutes * OneSecondInMilliseconds
-	OneHourInSeconds        = 1 * OneHourInMinutes * OneMinuteInSeconds
-)
