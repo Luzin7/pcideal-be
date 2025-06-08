87b23761e68241a0d3c59de20c3ee561dbd8f210
Luan Victor <lvictor.dutra@gmail.com>
2025-06-08 13:54:57 -0300

    feat: update GenerateBuilds method to return AIBuildResponse type

--- internal/contracts/google_ai_contract.go
+++ internal/contracts/google_ai_contract.go
@@ -1,6 +1,8 @@
 package contracts
 
+import "github.com/Luzin7/pcideal-be/internal/core/models"
+
 type GoogleAIContract interface {
 	BuildComputerPrompt(usageType string, cpuPreference string, gpuPreference string, budget int64) (string, error)
-	GenerateBuilds(prompt string) (map[string]interface{}, error)
+	GenerateBuilds(prompt string) (*models.AIBuildResponse, error)
 }
