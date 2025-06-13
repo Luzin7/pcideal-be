2c2897121010563aa8fbab4f143ca7e029e0fd28
Luan Victor <lvictor.dutra@gmail.com>
2025-06-13 15:22:31 -0300

    fix: load .env file conditionally based on environment

--- cmd/main.go
+++ cmd/main.go
@@ -15,10 +15,12 @@ import (
 )
 
 func main() {
-	err := godotenv.Load(".env")
+	if os.Getenv("ENV") != "production" {
+		err := godotenv.Load(".env")
 
-	if err != nil {
-		log.Fatal("Error loading .env file")
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
 	}
 
 	connectionString := os.Getenv("DATABASE_URL")
