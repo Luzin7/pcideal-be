dcccf4785504f4fce5664ebdf7ade52a21158a60
Luan Victor <lvictor.dutra@gmail.com>
2025-06-23 20:07:44 -0300

    feat: add UpdateProducts method to ScraperClient interface for bulk product updates

--- internal/contracts/scraper_contract.go
+++ internal/contracts/scraper_contract.go
@@ -5,4 +5,5 @@ import "github.com/Luzin7/pcideal-be/internal/core/models"
 type ScraperClient interface {
 	ScrapeAllCategories() ([]*models.Part, error)
 	ScrapeProduct(productLink string) (*models.Part, error)
+	UpdateProducts(urls []string) ([]*models.Part, error)
 }
