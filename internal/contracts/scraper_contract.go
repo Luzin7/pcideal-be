822dd92dc4617c8569a2e2213d95174f10c82cae
Luan Victor <lvictor.dutra@gmail.com>
2025-06-23 22:29:33 -0300

    feat: add dto to scraper

--- internal/contracts/scraper_contract.go
+++ internal/contracts/scraper_contract.go
@@ -1,9 +1,12 @@
 package contracts
 
-import "github.com/Luzin7/pcideal-be/internal/core/models"
+import (
+	"github.com/Luzin7/pcideal-be/infra/dto"
+	"github.com/Luzin7/pcideal-be/internal/core/models"
+)
 
 type ScraperClient interface {
 	ScrapeAllCategories() ([]*models.Part, error)
 	ScrapeProduct(productLink string) (*models.Part, error)
-	UpdateProducts(urls []string) ([]*models.Part, error)
+	UpdateProducts(urls []*dto.ProductLinkToUpdate) ([]*dto.PartWithID, error)
 }
