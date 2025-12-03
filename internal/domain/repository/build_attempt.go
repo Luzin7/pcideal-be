364c1886dae85ca93b2568a0c7879f99a1f5efb3
Luan Victor <lvictor.dutra@gmail.com>
2025-12-03 20:33:20 -0300

    refactor: update BuildAttemptRepository methods to accept context as a parameter

--- internal/domain/repository/build_attempt.go
+++ internal/domain/repository/build_attempt.go
@@ -1,13 +1,14 @@
 package repository
 
 import (
+	"context"
 	"time"
 
 	"github.com/Luzin7/pcideal-be/internal/domain/entity"
 )
 
 type BuildAttemptRepository interface {
-	CreateBuildAttempt(buildAttempt *entity.BuildAttempt) error
-	CountBuildAttemptsByIP(ip string, since time.Time) (int, error)
-	GetBuildAttemptsByIP(ip string, since time.Time) ([]*entity.BuildAttempt, error)
+	CreateBuildAttempt(ctx context.Context, buildAttempt *entity.BuildAttempt) error
+	CountBuildAttemptsByIP(ctx context.Context, ip string, since time.Time) (int, error)
+	GetBuildAttemptsByIP(ctx context.Context, ip string, since time.Time) ([]*entity.BuildAttempt, error)
 }
