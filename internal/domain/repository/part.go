2977dcf723a6f273b3978ecfdd92626aa3d09a9e
Luan Victor <lvictor.dutra@gmail.com>
2025-12-03 20:33:29 -0300

    refactor: update PartRepository methods to include context parameter

--- internal/domain/repository/part.go
+++ internal/domain/repository/part.go
@@ -1,11 +1,15 @@
 package repository
 
-import "github.com/Luzin7/pcideal-be/internal/domain/entity"
+import (
+	"context"
+
+	"github.com/Luzin7/pcideal-be/internal/domain/entity"
+)
 
 type PartRepository interface {
-	CreatePart(part *entity.Part) error
-	GetAllParts() ([]*entity.Part, error)
-	GetPartByID(id string) (*entity.Part, error)
-	GetPartByModel(model string) (*entity.Part, error)
-	UpdatePart(partId string, part *entity.Part) error
+	CreatePart(ctx context.Context, part *entity.Part) error
+	GetAllParts(ctx context.Context) ([]*entity.Part, error)
+	GetPartByID(ctx context.Context, id string) (*entity.Part, error)
+	GetPartByModel(ctx context.Context, model string) (*entity.Part, error)
+	UpdatePart(ctx context.Context, partId string, part *entity.Part) error
 }
