8fab13fe11f5bd82180ce76f2004d8c05b8678a5
Luan Victor <lvictor.dutra@gmail.com>
2025-12-03 20:33:23 -0300

    refactor: update GoogleAIRepository to include context in GeneratePcBuildAnalysis method

--- internal/domain/repository/google_ai.go
+++ internal/domain/repository/google_ai.go
@@ -1,8 +1,11 @@
 package repository
 
-import "github.com/Luzin7/pcideal-be/internal/domain/entity"
+import (
+	"context"
+
+	"github.com/Luzin7/pcideal-be/internal/domain/entity"
+)
 
 type GoogleAIRepository interface {
-	BuildComputerPrompt(usageType string, cpuPreference string, gpuPreference string, budget int64) (string, error)
-	GenerateBuilds(prompt string) (*entity.AIBuildResponse, error)
+	GeneratePcBuildAnalysis(ctx context.Context, part *entity.Part) (string, error)
 }
