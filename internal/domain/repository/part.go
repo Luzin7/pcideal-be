a4b0c0dd30bd975cdf0edd7ec5420efbaba68d9a
Luan Victor <lvictor.dutra@gmail.com>
2025-12-09 15:03:04 -0300

    refactor: update FindPartByTypeAndBrandWithMaxPrice method to use structured arguments

--- internal/domain/repository/part.go
+++ internal/domain/repository/part.go
@@ -6,11 +6,18 @@ import (
 	"github.com/Luzin7/pcideal-be/internal/domain/entity"
 )
 
+type FindPartByTypeAndBrandWithMaxPriceArgs struct {
+	PartType      entity.PartType
+	Brand         string
+	MaxPriceCents int64
+	MinPSUWatts   int16
+}
+
 type PartRepository interface {
 	CreatePart(ctx context.Context, part *entity.Part) error
 	GetAllParts(ctx context.Context) ([]*entity.Part, error)
 	GetPartByID(ctx context.Context, id string) (*entity.Part, error)
 	GetPartByModel(ctx context.Context, model string) (*entity.Part, error)
 	UpdatePart(ctx context.Context, partId string, part *entity.Part) error
-	FindPartByTypeAndBrandWithMaxPrice(ctx context.Context, partType, brand string, maxPriceCents int) (*entity.Part, error)
+	FindPartByTypeAndBrandWithMaxPrice(ctx context.Context, args FindPartByTypeAndBrandWithMaxPriceArgs) ([]*entity.Part, error)
 }
