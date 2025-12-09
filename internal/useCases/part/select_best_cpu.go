27330efacc5677122af72dab5bce465ffedc4250
Luan Victor <lvictor.dutra@gmail.com>
2025-12-09 15:03:11 -0300

    refactor: implement SelectBestCPUUseCase for selecting the best CPU based on brand and price

---
+++ internal/useCases/part/select_best_cpu.go
@@ -0,0 +1,46 @@
+package part
+
+import (
+	"context"
+
+	"github.com/Luzin7/pcideal-be/internal/domain/entity"
+	"github.com/Luzin7/pcideal-be/internal/domain/repository"
+)
+
+type SelectBestCPUUseCase struct {
+	partRepository repository.PartRepository
+}
+
+func NewSelectBestCPUUseCase(partRepository repository.PartRepository) *SelectBestCPUUseCase {
+	return &SelectBestCPUUseCase{
+		partRepository: partRepository,
+	}
+}
+
+type SelectBestCPUArgs struct {
+	brandPreference string
+	maxPriceCents   int64
+}
+
+func (uc *SelectBestCPUUseCase) Execute(cpuPreference SelectBestCPUArgs) (entity.Part, error) {
+	ctx := context.TODO()
+
+	cpus, err := uc.partRepository.FindPartByTypeAndBrandWithMaxPrice(ctx, repository.FindPartByTypeAndBrandWithMaxPriceArgs{
+		PartType:      "CPU",
+		Brand:         cpuPreference.brandPreference,
+		MaxPriceCents: cpuPreference.maxPriceCents,
+	})
+	if err != nil {
+		return entity.Part{}, err //TODO: adicionar erro custom depois
+	}
+
+	var bestCPU entity.Part
+
+	for i, cpu := range cpus {
+		if i == 0 || cpu.Specs.PerformanceScore >= bestCPU.Specs.PerformanceScore && cpu.PriceCents <= cpuPreference.maxPriceCents {
+			bestCPU = *cpu
+		}
+	}
+
+	return bestCPU, nil
+}
