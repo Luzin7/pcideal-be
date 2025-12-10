6b84fa1077e84ea9645b92daac180ee47952d49e
Luan Victor <lvictor.dutra@gmail.com>
2025-12-09 21:48:46 -0300

    refactor: reorganize budget strategy types and improve allocation logic

--- internal/domain/entity/budget_strategy.go
+++ internal/domain/entity/budget_strategy.go
@@ -5,17 +5,87 @@ type BudgetStrategy interface {
 	GetName() string
 }
 
-type GamerStrategy struct{}
+type GamingLowBudgetStrategy struct{}
 
-func (s GamerStrategy) GetName() string { return "GAMING" }
-func (s GamerStrategy) GetAllocations() map[PartType]float64 {
+func (s GamingLowBudgetStrategy) GetName() string { return "GAMING" }
+func (s GamingLowBudgetStrategy) GetAllocations() map[PartType]float64 {
+	return map[PartType]float64{
+		TypeGPU:  0.40,
+		TypeCPU:  0.20,
+		TypeMobo: 0.14,
+		TypeRAM:  0.10,
+		TypePSU:  0.10,
+		TypeSSD:  0.06,
+	}
+}
+
+type GamingMidBudgetStrategy struct{}
+
+func (s GamingMidBudgetStrategy) GetName() string { return "GAMING" }
+func (s GamingMidBudgetStrategy) GetAllocations() map[PartType]float64 {
 	return map[PartType]float64{
 		TypeGPU:  0.45,
-		TypeCPU:  0.20,
-		TypeMobo: 0.10,
-		TypeRAM:  0.08,
+		TypeCPU:  0.18,
+		TypeMobo: 0.12,
+		TypeRAM:  0.10,
+		TypePSU:  0.08,
+		TypeSSD:  0.07,
+	}
+}
+
+type GamingHighBudgetStrategy struct{}
+
+func (s GamingHighBudgetStrategy) GetName() string { return "GAMING" }
+func (s GamingHighBudgetStrategy) GetAllocations() map[PartType]float64 {
+	return map[PartType]float64{
+		TypeGPU:  0.50,
+		TypeCPU:  0.15,
+		TypeMobo: 0.12,
+		TypeRAM:  0.10,
 		TypePSU:  0.07,
-		TypeSSD:  0.05,
+		TypeSSD:  0.06,
+	}
+}
+
+type WorkLowBudgetStrategy struct{}
+
+func (s WorkLowBudgetStrategy) GetName() string { return "WORK" }
+func (s WorkLowBudgetStrategy) GetAllocations() map[PartType]float64 {
+	return map[PartType]float64{
+		TypeGPU:  0.00,
+		TypeCPU:  0.45,
+		TypeRAM:  0.20,
+		TypeSSD:  0.10,
+		TypeMobo: 0.15,
+		TypePSU:  0.10,
+	}
+}
+
+type WorkMidBudgetStrategy struct{}
+
+func (s WorkMidBudgetStrategy) GetName() string { return "WORK" }
+func (s WorkMidBudgetStrategy) GetAllocations() map[PartType]float64 {
+	return map[PartType]float64{
+		TypeGPU:  0.00,
+		TypeCPU:  0.40,
+		TypeRAM:  0.25,
+		TypeSSD:  0.12,
+		TypeMobo: 0.15,
+		TypePSU:  0.08,
+	}
+}
+
+type WorkHighBudgetStrategy struct{}
+
+func (s WorkHighBudgetStrategy) GetName() string { return "WORK" }
+func (s WorkHighBudgetStrategy) GetAllocations() map[PartType]float64 {
+	return map[PartType]float64{
+		TypeGPU:  0.25,
+		TypeCPU:  0.35,
+		TypeRAM:  0.20,
+		TypeSSD:  0.10,
+		TypeMobo: 0.05,
+		TypePSU:  0.05,
 	}
 }
 
@@ -33,27 +103,28 @@ func (s OfficeStrategy) GetAllocations() map[PartType]float64 {
 	}
 }
 
-type WorkstationStrategy struct{}
+func GetStrategy(objective string, budget int64) BudgetStrategy {
+	const (
+		lowBudgetThreshold  = 350000
+		highBudgetThreshold = 700000
+	)
 
-func (s WorkstationStrategy) GetName() string { return "WORK" }
-func (s WorkstationStrategy) GetAllocations() map[PartType]float64 {
-	return map[PartType]float64{
-		TypeGPU:  0.20,
-		TypeCPU:  0.35,
-		TypeRAM:  0.20,
-		TypeSSD:  0.10,
-		TypeMobo: 0.10,
-		TypePSU:  0.05,
-	}
-}
-
-func GetStrategy(objective string) BudgetStrategy {
 	switch objective {
 	case "WORK":
-		return WorkstationStrategy{}
+		if budget < lowBudgetThreshold {
+			return WorkLowBudgetStrategy{}
+		} else if budget < highBudgetThreshold {
+			return WorkMidBudgetStrategy{}
+		}
+		return WorkHighBudgetStrategy{}
 	case "OFFICE":
 		return OfficeStrategy{}
-	default:
-		return GamerStrategy{}
+	default: // "GAMING"
+		if budget < lowBudgetThreshold {
+			return GamingLowBudgetStrategy{}
+		} else if budget < highBudgetThreshold {
+			return GamingMidBudgetStrategy{}
+		}
+		return GamingHighBudgetStrategy{}
 	}
 }
