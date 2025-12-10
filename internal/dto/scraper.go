4204454d81a355147b5dc724286b538d6037e646
Luan Victor <lvictor.dutra@gmail.com>
2025-12-09 21:49:17 -0300

    refactor: add ProductLinkToUpdate and PartWithID structs for DTO organization

---
+++ internal/dto/scraper.go
@@ -0,0 +1,13 @@
+package dto
+
+import "github.com/Luzin7/pcideal-be/internal/domain/entity"
+
+type ProductLinkToUpdate struct {
+	ID  string `json:"id"`
+	URL string `json:"url"`
+}
+
+type PartWithID struct {
+	ID   string       `json:"id"`
+	Part *entity.Part `json:"part"`
+}
