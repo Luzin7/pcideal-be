cba9a15f7ba35bc9b2c793b216ca0f9bdf6f17b9
Luan Victor <lvictor.dutra@gmail.com>
2025-12-09 21:49:03 -0300

    refactor: add JSON tags to Specs and Part structs for improved serialization

--- internal/domain/entity/part.go
+++ internal/domain/entity/part.go
@@ -18,32 +18,31 @@ const (
 )
 
 type Specs struct {
-	Socket           string `bson:"socket,omitempty"`
-	MemoryType       string `bson:"memory_type,omitempty"`
-	FormFactor       string `bson:"form_factor,omitempty"`
-	MinPSUWatts      int    `bson:"min_psu_watts,omitempty"`
-	Wattage          int    `bson:"wattage,omitempty"`
-	EfficiencyRating string `bson:"efficiency_rating,omitempty"`
-	PerformanceScore int    `bson:"performance_score,omitempty"`
-	TierScore        string `bson:"tier_score,omitempty"`
-	VramGB           int    `bson:"vram_gb,omitempty"`
-	CapacityGB       int    `bson:"capacity_gb,omitempty"`
-	MemorySpeedMHz   int    `bson:"memory_speed_mhz,omitempty"`
-	CasLatency       int    `bson:"cas_latency,omitempty"`
+	Socket           string `bson:"socket,omitempty" json:"socket,omitempty"`
+	MemoryType       string `bson:"memory_type,omitempty" json:"memory_type,omitempty"`
+	FormFactor       string `bson:"form_factor,omitempty" json:"form_factor,omitempty"`
+	MinPSUWatts      int16  `bson:"min_psu_watts,omitempty" json:"min_psu_watts,omitempty"`
+	Wattage          int16  `bson:"wattage,omitempty" json:"wattage,omitempty"`
+	EfficiencyRating int8   `bson:"efficiency_rating,omitempty" json:"efficiency_rating,omitempty"`
+	PerformanceScore int8   `bson:"performance_score,omitempty" json:"performance_score,omitempty"`
+	TierScore        int8   `bson:"tier_score,omitempty" json:"tier_score,omitempty"`
+	VramGB           int    `bson:"vram_gb,omitempty" json:"vram_gb,omitempty"`
+	CapacityGB       int    `bson:"capacity_gb,omitempty" json:"capacity_gb,omitempty"`
+	MemorySpeedMHz   int    `bson:"memory_speed_mhz,omitempty" json:"memory_speed_mhz,omitempty"`
+	CasLatency       int    `bson:"cas_latency,omitempty" json:"cas_latency,omitempty"`
 }
 
 type Part struct {
-	ID            primitive.ObjectID `bson:"_id,omitempty"`
-	Type          PartType           `bson:"type"`
-	Brand         string             `bson:"brand"`
-	Model         string             `bson:"model"`
-	URL           string             `bson:"url"`
-	Store         string             `bson:"store"`
-	AffiliatedURL string             `bson:"affiliated_url"`
-	PriceCents    int64              `bson:"price_cents"`
-	Specs         Specs              `bson:"specs"`
-	IsParsed      bool               `bson:"is_parsed"`
-	UpdatedAt     time.Time          `bson:"updated_at"`
+	ID            primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Type          PartType           `bson:"type" json:"type"`
+	Brand         string             `bson:"brand" json:"brand"`
+	Model         string             `bson:"model" json:"model"`
+	URL           string             `bson:"url" json:"url"`
+	Store         string             `bson:"store" json:"store"`
+	AffiliatedURL string             `bson:"affiliate_url" json:"affiliate_url"`
+	PriceCents    int64              `bson:"price_cents" json:"price_cents"`
+	Specs         Specs              `bson:"specs" json:"specs"`
+	UpdatedAt     time.Time          `bson:"updated_at" json:"updated_at"`
 }
 
 func NewPart(ty PartType, brand string, model string, url string, store string, affiliatedURL string, priceCents int64, specs Specs) *Part {
@@ -57,7 +56,6 @@ func NewPart(ty PartType, brand string, model string, url string, store string, 
 		AffiliatedURL: affiliatedURL,
 		PriceCents:    priceCents,
 		Specs:         specs,
-		IsParsed:      true,
 		UpdatedAt:     time.Now(),
 	}
 }
