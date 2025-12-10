40376279556723328c19a9deb880140aaadc395f
Luan Victor <lvictor.dutra@gmail.com>
2025-12-09 21:49:29 -0300

    refactor: enhance SelectBestGPUUseCase with improved error handling and logging

--- internal/useCases/part/select_best_gpu.go
+++ internal/useCases/part/select_best_gpu.go
@@ -2,9 +2,11 @@ package part
 
 import (
 	"context"
+	"log"
 
 	"github.com/Luzin7/pcideal-be/internal/domain/entity"
 	"github.com/Luzin7/pcideal-be/internal/domain/repository"
+	"github.com/Luzin7/pcideal-be/internal/errors"
 )
 
 type SelectBestGPUUseCase struct {
@@ -22,24 +24,49 @@ type SelectBestGPUArgs struct {
 	MaxPriceCents   int64
 }
 
-func (uc *SelectBestGPUUseCase) Execute(ctx context.Context, gpuPreference SelectBestGPUArgs) (entity.Part, error) {
+func (uc *SelectBestGPUUseCase) Execute(ctx context.Context, gpuPreference SelectBestGPUArgs) (entity.Part, *errors.ErrService) {
+	log.Printf("[SelectBestGPU] Filtering - Brand: %s, MaxPrice: %d", gpuPreference.BrandPreference, gpuPreference.MaxPriceCents)
+
 	gpus, err := uc.partRepository.FindPartByTypeAndBrandWithMaxPrice(ctx, repository.FindPartByTypeAndBrandWithMaxPriceArgs{
 		PartType:      "GPU",
 		Brand:         gpuPreference.BrandPreference,
 		MaxPriceCents: gpuPreference.MaxPriceCents,
 	})
 	if err != nil {
-		return entity.Part{}, err //TODO: adicionar erro custom depois
+		log.Printf("[SelectBestGPU] Error querying database: %v", err)
+		return entity.Part{}, errors.New("Failed to select best GPU", 500)
+	}
+
+	log.Printf("[SelectBestGPU] Found %d GPUs", len(gpus))
+
+	if len(gpus) == 0 {
+		log.Printf("[SelectBestGPU] No GPUs found matching criteria")
+		return entity.Part{}, errors.New("No GPU found matching the criteria", 404)
 	}
 
 	var bestGPU entity.Part
 
 	for i, gpu := range gpus {
-		if i == 0 || (gpu.Specs.PerformanceScore > bestGPU.Specs.PerformanceScore ||
-			(gpu.Specs.PerformanceScore == bestGPU.Specs.PerformanceScore && gpu.PriceCents < bestGPU.PriceCents)) {
+		if i == 0 {
+			bestGPU = *gpu
+			continue
+		}
+
+		if gpu.Specs.PerformanceScore > bestGPU.Specs.PerformanceScore {
+			bestGPU = *gpu
+			continue
+		}
+
+		if gpu.Specs.PerformanceScore < bestGPU.Specs.PerformanceScore {
+			continue
+		}
+
+		if gpu.PriceCents < bestGPU.PriceCents {
 			bestGPU = *gpu
 		}
 	}
+
+	log.Printf("[SelectBestGPU] Selected: %s (Brand: %s, Price: %d, Score: %d, MinPSU: %d)", bestGPU.Model, bestGPU.Brand, bestGPU.PriceCents, bestGPU.Specs.PerformanceScore, bestGPU.Specs.MinPSUWatts)
 
 	return bestGPU, nil
 }
