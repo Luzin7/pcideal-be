07e7de913243797969963d660bac66d011fcd9b2
Luan Victor <lvictor.dutra@gmail.com>
2025-12-11 18:29:45 -0300

    refactor: add parts update logic to SelectBestPSUUseCase

--- internal/useCases/part/select_best_psu.go
+++ internal/useCases/part/select_best_psu.go
@@ -6,7 +6,9 @@ import (
 
 	"github.com/Luzin7/pcideal-be/internal/domain/entity"
 	"github.com/Luzin7/pcideal-be/internal/domain/repository"
+	"github.com/Luzin7/pcideal-be/internal/dto"
 	"github.com/Luzin7/pcideal-be/internal/errors"
+	"github.com/Luzin7/pcideal-be/internal/util"
 )
 
 type SelectBestPSUUseCase struct {
@@ -25,11 +27,19 @@ type SelectBestPSUArgs struct {
 
 func (uc *SelectBestPSUUseCase) Execute(ctx context.Context, args SelectBestPSUArgs) (entity.Part, *errors.ErrService) {
 	var bestPSU entity.Part
+	var partsToUpdate []dto.ProductLinkToUpdate
 
 	for i, psu := range args.psus {
 		if i == 0 {
 			bestPSU = *psu
 			continue
+		}
+
+		if util.PartNeedToUpdate(psu) {
+			partsToUpdate = append(partsToUpdate, dto.ProductLinkToUpdate{
+				ID:  psu.ID.Hex(),
+				Url: psu.URL,
+			})
 		}
 
 		if psu.Specs.EfficiencyRating > bestPSU.Specs.EfficiencyRating {
@@ -44,6 +54,12 @@ func (uc *SelectBestPSUUseCase) Execute(ctx context.Context, args SelectBestPSUA
 		if psu.PriceCents < bestPSU.PriceCents {
 			bestPSU = *psu
 		}
+	}
+
+	if len(partsToUpdate) > 0 {
+		go func() {
+			uc.partRepository.UpdateParts(context.Background(), partsToUpdate, "kabum")
+		}()
 	}
 
 	log.Printf("[SelectBestPSU] Selected: %s (Brand: %s, Price: %d, Wattage: %d, Efficiency: %d)", bestPSU.Model, bestPSU.Brand, bestPSU.PriceCents, bestPSU.Specs.Wattage, bestPSU.Specs.EfficiencyRating)
