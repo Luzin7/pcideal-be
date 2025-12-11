afb3a39e77ce3debce0fa5e1f555132267a5f42b
Luan Victor <lvictor.dutra@gmail.com>
2025-12-11 18:29:22 -0300

    refactor: add UpdateParts method to PartRepository for batch updates

--- internal/domain/repository/part.go
+++ internal/domain/repository/part.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/Luzin7/pcideal-be/internal/domain/entity"
+	"github.com/Luzin7/pcideal-be/internal/dto"
 )
 
 type FindPartByTypeAndBrandWithMaxPriceArgs struct {
@@ -21,5 +22,6 @@ type PartRepository interface {
 	GetPartByID(ctx context.Context, id string) (*entity.Part, error)
 	GetPartByModel(ctx context.Context, model string) (*entity.Part, error)
 	UpdatePart(ctx context.Context, partId string, part *entity.Part) error
+	UpdateParts(ctx context.Context, urls []dto.ProductLinkToUpdate, store string) error
 	FindPartByTypeAndBrandWithMaxPrice(ctx context.Context, args FindPartByTypeAndBrandWithMaxPriceArgs) ([]*entity.Part, error)
 }
