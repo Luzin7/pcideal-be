91d035aadf3713ec0315319ee5fc4f03a440081d
Luan Victor <lvictor.dutra@gmail.com>
2025-12-11 18:29:38 -0300

    refactor: enhance SelectBestGPUUseCase to include parts update logic

--- internal/useCases/part/select_best_gpu.go
+++ internal/useCases/part/select_best_gpu.go
@@ -6,7 +6,9 @@ import (
 
 	"github.com/Luzin7/pcideal-be/internal/domain/entity"
 	"github.com/Luzin7/pcideal-be/internal/domain/repository"
+	"github.com/Luzin7/pcideal-be/internal/dto"
 	"github.com/Luzin7/pcideal-be/internal/errors"
+	"github.com/Luzin7/pcideal-be/internal/util"
 )
 
 type SelectBestGPUUseCase struct {
@@ -25,11 +27,19 @@ type SelectBestGPUArgs struct {
 
 func (uc *SelectBestGPUUseCase) Execute(ctx context.Context, args SelectBestGPUArgs) (entity.Part, *errors.ErrService) {
 	var bestGPU entity.Part
+	var partsToUpdate []dto.ProductLinkToUpdate
 
 	for i, gpu := range args.gpus {
 		if i == 0 {
 			bestGPU = *gpu
 			continue
+		}
+
+		if util.PartNeedToUpdate(gpu) {
+			partsToUpdate = append(partsToUpdate, dto.ProductLinkToUpdate{
+				ID:  gpu.ID.Hex(),
+				Url: gpu.URL,
+			})
 		}
 
 		if gpu.Specs.PerformanceScore > bestGPU.Specs.PerformanceScore {
@@ -44,6 +54,12 @@ func (uc *SelectBestGPUUseCase) Execute(ctx context.Context, args SelectBestGPUA
 		if gpu.PriceCents < bestGPU.PriceCents {
 			bestGPU = *gpu
 		}
+	}
+
+	if len(partsToUpdate) > 0 {
+		go func() {
+			uc.partRepository.UpdateParts(context.Background(), partsToUpdate, "kabum")
+		}()
 	}
 
 	log.Printf("[SelectBestGPU] Selected: %s (Brand: %s, Price: %d, Score: %d, MinPSU: %d)", bestGPU.Model, bestGPU.Brand, bestGPU.PriceCents, bestGPU.Specs.PerformanceScore, bestGPU.Specs.MinPSUWatts)
