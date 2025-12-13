2b9e62726aa6964ec2bd3848af368e0667690652
Luan Victor <lvictor.dutra@gmail.com>
2025-12-13 15:42:51 -0300

    refactor: add UpdateProductsResponse struct for improved product update handling

--- internal/dto/scraper.go
+++ internal/dto/scraper.go
@@ -11,3 +11,11 @@ type PartWithID struct {
 	ID   string       `json:"id"`
 	Part *entity.Part `json:"part"`
 }
+
+type UpdateProductsResponse struct {
+	Store   string        `json:"store"`
+	Total   int           `json:"total"`
+	Success int           `json:"success"`
+	Failed  int           `json:"failed"`
+	Results []*PartWithID `json:"results"`
+}
